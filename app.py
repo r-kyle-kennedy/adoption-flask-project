4967d217ae1bca21f54cd3b661b3e92428d006fd
r-kyle-kennedy <r.kyle.kennedy@gmail.com>
2021-11-03 16:21:07 -0700

    broke up html templates to be more dry

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
-
-
-app = Flask(__name__)
+from flask import (render_template,
+                    url_for, request)
+from models import db, Pet, app
 
 
 @app.route('/')
 def index():
-    return 'Hello from Pet Adoption'
+    return render_template('index.html')
 
+
+@app.route('/add-pet', methods=['GET', 'POST'])
+def add_pet():
+    print(request.form)
+    return render_template('addpet.html')
+
+
+@app.route('/pet')
+def pet():
+    return render_template('pet.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='127.0.0.1')
+    db.create_all()
+    app.run(debug=True, port=8000, host='0.0.0.0')
