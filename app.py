921ec18bcba5cedc32f44498e96a5c6e84a1b53b
r-kyle-kennedy <r.kyle.kennedy@gmail.com>
2021-11-03 17:52:08 -0700

    add and edit pets

--- app.py
+++ app.py
@@ -1,22 +1,56 @@
-from flask import (render_template,
+from flask import (render_template, redirect,
                     url_for, request)
 from models import db, Pet, app
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets)
 
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
-    print(request.form)
+    if request.form:
+        new_pet = Pet(name=request.form['name'], age=request.form['age'],
+                  breed=request.form['breed'], color=request.form['color'],
+                  size=request.form['size'], weight=request.form['weight'],
+                  url=request.form['url'], url_tag=request.form['alt'],
+                  pet_type=request.form['pet'], gender=request.form['gender'],
+                  spay=request.form['spay'], house_trained=request.form['housetrained'],
+                  description=request.form['description'])
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('addpet.html')
 
 
-@app.route('/pet')
-def pet():
-    return render_template('pet.html')
+@app.route('/pet/<id>')
+def pet(id):
+    pet = Pet.query.get(id)
+    return render_template('pet.html', pet=pet)
+
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_pet(id):
+    pet = Pet.query.get(id)
+    if request.form:
+        pet.name = request.form['name']
+        pet.age = request.form['age']
+        pet.breed = request.form['breed']
+        pet.color = request.form['color']
+        pet.size = request.form['size']
+        pet.weight = request.form['weight']
+        pet.url = request.form['url']
+        pet.url_tag = request.form['alt']
+        pet.pet_type = request.form['pet']
+        pet.gender = request.form['gender']
+        pet.spay = request.form['spay']
+        pet.house_trained = request.form['housetrained']
+        pet.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('editpet.html', pet=pet)
 
 
 if __name__ == '__main__':
